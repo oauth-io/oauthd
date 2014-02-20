f20488540424bd3fbcb604f0612f5f3fdb9056d6
Antoine Jackson <a.j.william26@gmail.com>
2014-02-21 00:28:48 +0100

    added proxy for http requests in server.front

--- plugins/server.front/proxy/js/index.js
+++ plugins/server.front/proxy/js/index.js
@@ -1,6 +1,35 @@
+var request, transform_urls, url;
+
+url = require('url');
+
+request = require('request');
+
+transform_urls = function(data, host) {
+  data = data.replace(/src=[\"'](\/.+?)[\"']/g, 'src="' + '/proxy?url=' + host + '$1' + '"');
+  data = data.replace(/href=[\"'](\/.+?)[\"']/g, 'href="' + '/proxy?url=' + host + '$1' + '"');
+  data = data.replace(/src=[\"'](http:.+?)[\"']/g, 'src="' + '/proxy?url=' + '$1' + '"');
+  data = data.replace(/href=[\"'](http:.+?)[\"']/g, 'href="' + '/proxy?url=' + '$1' + '"');
+  data = data.replace(/[\"'](http:.+?)[\"']/g, '"' + '/proxy?url=' + '$1' + '"');
+  return data;
+};
+
 module.exports = function(req, res, next) {
-  console.log('Entered proxy mode');
-  console.log('params', req.params);
-  console.log('body', req.body);
-  return console.log('hello');
+  var options, urlDecoded;
+  url = url.parse(req.params.url);
+  urlDecoded = decodeURIComponent(req.params.url);
+  options = {
+    uri: urlDecoded,
+    method: 'GET',
+    'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.107 Safari/537.36'
+  };
+  return request(options, function(err, response, body) {
+    var result;
+    result = transform_urls(body, url.protocol + '//' + url.host);
+    res.writeHead(200, {
+      'Content-Length': Buffer.byteLength(result),
+      'Content-Type': response.headers['content-type']
+    });
+    res.write(result);
+    return res.end();
+  });
 };
