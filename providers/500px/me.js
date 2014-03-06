77f56cd70487a68b0cf22ab7cf44a21021881ded
Antoine Jackson <a.j.william26@gmail.com>
2014-03-06 01:08:32 +0100

    added me.js examples for 23andme and 500px

---
+++ providers/500px/me.js
@@ -0,0 +1,36 @@
+var me = {
+    fetch: [
+
+        function(fetched_elts) {
+            return '/v1/users';
+        }
+
+    ],
+    params: {},
+    fields: {
+        name: function(me) {
+            return me.user.firstname || me.user.lastname ? me.user.firstname + ' ' +
+                me.user.lastname : me.user.fullname;
+        },
+        firstname: function(me) {
+            return me.user.firstname;
+        },
+        lastname: function(me) {
+            return me.user.lastname;
+        },
+        email: function(me) {
+            return me.user.email;
+        },
+        avatar: function(me) {
+            return me.user.avatars.
+            default.http;
+        },
+        location: function(me) {
+            return me.user.city + ', ' + me.user.country
+        },
+        raw: function(me) {
+            return me;
+        }
+    }
+};
+module.exports = me;
