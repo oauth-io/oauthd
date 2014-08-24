ddc15406aea7f0b3f6179ed226eedb3002f8efc8
Chia-liang Kao <clkao@clkao.org>
2014-08-24 14:47:13 +0800

    Add provider for Uber

---
+++ providers/uber/me.js
@@ -0,0 +1,14 @@
+var me = {
+
+    url: '/v1/me',
+    params: {},
+    fields: {
+        name: '=',
+        firstname: 'first_name',
+        lastname: 'last_name',
+        avatar: 'picture',
+        email: '=',
+    }
+};
+
+module.exports = me;
