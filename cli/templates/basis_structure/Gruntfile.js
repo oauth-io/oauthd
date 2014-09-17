c9ba3f1187c18652f7ccef48e5993542b9e3c784
Vincent Lefebvre <vinc.lefebv@gmail.com>
2014-09-17 17:41:09 +0200

    update file in basis structure to match current architecture.

---
+++ cli/templates/basis_structure/Gruntfile.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var fs = require('fs');
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    var gruntConf = {
+        watch: {
+            options: {
+                nospawn: true
+            }
+        },
+        coffee: {},
+        subgrunt: {}
+    };
+
+    var tasks = [];
+
+    var added_plg = fs.readdirSync(process.cwd() + '/plugins');
+    for (var i in added_plg) {
+        var plugin = added_plg[i];
+        if (fs.existsSync(process.cwd() + '/plugins/' + plugin + '/gruntConfig.js')) {
+            var task = require('./plugins/' + plugin + '/gruntConfig').call(this, gruntConf);
+            if (task)
+                tasks.push(task);
+        }
+        if (fs.existsSync(process.cwd() + '/plugins/' + plugin + '/Gruntfile.js')) {
+            gruntConf.subgrunt[plugin] = {
+                options: {},
+                projects: {}
+            };
+            gruntConf.subgrunt[plugin].projects['./plugins/' + plugin] =  'default';
+        }
+    }
+
+    grunt.initConfig(gruntConf);
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-coffee');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-subgrunt');
+
+    // Default task.
+    grunt.registerTask('default', ['coffee', 'subgrunt']);
+    grunt.registerTask('test', ['nodeunit']);
+};
--- cli/templates/basis_structure/config.js
+++ cli/templates/basis_structure/config.js
@@ -1,5 +1,6 @@
 var config = {
-	
+	plugins: [
+	]
 };
 
 module.exports = config;
