26bfd22236c31db3cd289491688756398cb164fd
dreadjr <dreadjr@users.noreply.github.com>
2014-09-29 14:29:51 -0700

    add id to me()

--- providers/linkedin/me.js
+++ providers/linkedin/me.js
@@ -1,8 +1,9 @@
 var me = {
 
-    url: '/v1/people/~:(first-name,last-name,headline,picture-url)?format=json',
+    url: '/v1/people/~:(id,first-name,last-name,headline,picture-url)?format=json',
     params: {},
     fields: {
+        id: 'id',
         name: function(me) {
             return me.firstName + ' ' + me.lastName;
         },
