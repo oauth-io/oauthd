78b97759f824470536dfdf360f35b982ec17ac33
Sjimi <dimitri@darwinanalytics.com>
2014-09-30 17:23:07 +0200

    Add Campaign Monitor with all the necessary bells and whistles

---
+++ providers/campaign_monitor/me.js
@@ -0,0 +1,14 @@
+var me = {
+    fetch: [
+
+        function(fetched_elts) {
+            return '/primarycontact.json';
+        }
+
+    ],
+    params: {},
+    fields: {
+        email: 'EmailAddress'
+    }
+};
+module.exports = me;
