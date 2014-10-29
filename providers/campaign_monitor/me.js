29ef64c74c200d7bc4e75446cc051cfe9730e01c
Vincent Lefebvre <vinc.lefebv@gmail.com>
2014-10-29 12:48:25 +0100

    add campaign monitor me and update conf.json

---
+++ providers/campaign_monitor/me.js
@@ -0,0 +1,15 @@
+var me = {
+    fetch: [
+
+        function(fetched_elts) {
+            return '/primarycontact.json';
+        }
+
+    ],
+    params: {},
+    fields: {
+        email: 'EmailAddress'
+    }
+};
+
+module.exports = me;
