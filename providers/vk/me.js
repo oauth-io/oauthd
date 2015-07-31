88f4ae6e26f8c455ab40d6d272e4aec68ebe922d
Timur Salyakhutdinov <timur.salyakhutdinov@setinbox.com>
2015-07-31 15:31:06 +0300

    Extend slashme info for vk provider

--- providers/vk/me.js
+++ providers/vk/me.js
@@ -6,8 +6,13 @@ var me = {
         }
 
     ],
-    params: {},
+    params: {
+        fields: 'bdate,sex,photo_max_orig,contacts'
+    },
     fields: {
+        id: function(me) {
+            return me.response && me.response[0] && me.response[0].uid || undefined;
+        },
         name: function(me) {
             return me.response && me.response[0] ? me.response[0].first_name + ' ' + me.response[0].last_name : undefined;
         },
@@ -16,6 +21,33 @@ var me = {
         },
         lastname: function(me) {
             return me.response && me.response[0] ? me.response[0].last_name : undefined;
+        },
+        gender: function(me) {
+            if (me.response && me.response[0] && me.response[0].sex) {
+                return me.response[0].sex == 2 ? 1: 0;
+            }
+            return undefined;
+        },
+        birthdate: function(me) {
+            var bdate = me.response && me.response[0] && me.response[0].bdate || undefined;
+            if (bdate) {
+                var bdayArray = bdate.split(".");
+                return {
+                    day: bdayArray[0],
+                    month: bdayArray[1],
+                    year: bdayArray[2]
+                }
+            }
+            return undefined;
+        },
+        avatar: function(me) {
+            return me.response && me.response[0] && me.response[0].photo_max_orig || undefined;
+        },
+        phone: function(me) {
+            return me.response && me.response[0] && me.response[0].photo_max_orig || undefined;
+        },
+        url: function(me) {
+            return "https://vk.com/id" + me.id;
         }
     }
 };
