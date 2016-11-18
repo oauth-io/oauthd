b9a1b62826b35647e302c08e319a54531906bbcf
Thibaud Arnault <thibaud.arnault@gmail.com>
2016-11-17 22:34:36 -0400

    fix typo in generated config.js file

--- src/scaffolding/templates/basis_structure/config.js
+++ src/scaffolding/templates/basis_structure/config.js
@@ -2,9 +2,9 @@
 
 var config = {
 	host_url: process.env.oauthd_host_url || "http://localhost:6284",						// mounted on this url
-	port: process.env.oauthd_port || 6284,												// the port your instance is supposed to run on
-	//http_port: 6285,										// http port for redirection if using SLL
-	// bind: "127.0.0.1",									// bind to an ip
+	port: process.env.oauthd_port || 6284,					// The port your instance is supposed to run on
+	// http_port: 6285,										// HTTP port for redirection if using SSL
+	// bind: "127.0.0.1",									// Bind to an ip
 
 
 	staticsalt: 'i m a random string, change me.',			// used in hash generation, change for more security
@@ -13,8 +13,8 @@ var config = {
 	redis: {												// your redis configuration
 		port: 6379,
 		host: '127.0.0.1',
-		// password: '...my redis password...',
-		// database: ...0~15...
+		// password: 'my redis password',
+		// database: 0										// The Redis Database (a number between 0 & 15)
 		// options: {...other options...}
 	},
 
