e42dc6d8e1a18df1e68803792e7b2692520c2ed1
Thibaud Arnault <thibaud.arnault@gmail.com>
2016-12-06 02:50:18 -0400

    Twitter me() includes email if defined

--- providers/twitter/me.js
+++ providers/twitter/me.js
@@ -1,21 +1,21 @@
 var me = {
-
-    url: '/1.1/account/verify_credentials.json',
-    params: {},
-    fields: {
-    	  id: 'id_str',
-        name: '=',
-        alias: 'screen_name',
-        bio: 'description',
-        avatar: 'profile_image_url_https',
-        location: '=',
-        language: 'lang',
-        timezone: 'time_zone',
-        website: 'url',
-        url: function(me) {
-            return 'https://twitter.com/' + me.screen_name;
-        }
-    }
+	url: '/1.1/account/verify_credentials.json?include_email=true',
+	params: {},
+	fields: {
+		id: 'id_str',
+		name: '=',
+		alias: 'screen_name',
+		email: '=',
+		bio: 'description',
+		avatar: 'profile_image_url_https',
+		location: '=',
+		language: 'lang',
+		timezone: 'time_zone',
+		website: 'url',
+		url: function(me) {
+			return 'https://twitter.com/' + me.screen_name;
+		}
+	}
 };
 
 module.exports = me;
