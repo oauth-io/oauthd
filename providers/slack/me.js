1861f8e0e70b949f83c80a3dc74357d2914433fa
Thibaud Arnault <thibaud.arnault@gmail.com>
2017-05-23 15:05:12 -0400

    fix slack me() (if missing information - cf #198)

--- providers/slack/me.js
+++ providers/slack/me.js
@@ -19,22 +19,34 @@ var me = {
   params: {},
   fields: {
     id: function (me) {
-      return me.user.id;
+      if (me && me.user) {
+        return me.user && me.user.id;
+      }
     },
     avatar: function (me) {
-      return me.user.profile.image_48;
+      if (me && me.user) {
+        return me.user.profile.image_48;
+      }
     },
     name: function (me) {
-      return me.user.real_name;
+      if (me && me.user) {
+        return me.user.real_name;
+      }
     },
     firstname: function (me) {
-      return me.user.profile.first_name;
+      if (me && me.user && me.user.profile) {
+        return me.user.profile.first_name;
+      }
     },
     lastname: function (me) {
-      return me.user.profile.last_name;
+      if (me && me.user && me.user.profile) {
+        return me.user.profile.last_name;
+      }
     },
     email: function (me) {
-      return me.user.profile.email;
+      if (me && me.user && me.user.profile) {
+        return me.user.profile.email;
+      }
     }
   }
 };
