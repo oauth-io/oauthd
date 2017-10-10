772c86209deb2bf74528c844741ba98942d52b76
Thibaud Arnault <thibaud.arnault@gmail.com>
2017-10-10 08:38:55 -0400

    Dropbox: Fix request + me feature for api v2
    require slashme plugin v1.0.1 to handle method in me.js files

--- providers/dropbox/me.js
+++ providers/dropbox/me.js
@@ -1,17 +1,32 @@
 var me = {
     fetch: [
-
+		{
+			url: '/users/get_current_account',
+			method: 'post',
+			export: {
+                name: function(result) { return result.name	},
+				email: function(result) { return result.email },
+				country: function(result) { return result.country },
+				locale: function(result) { return result.locale },
+				avatar: function(result) { return result.profile_photo_url }
+            }
+		},
         function(fetched_elts) {
-            return '/1/account/info';
+            return fetched_elts;
         }
-
     ],
     params: {},
     fields: {
-        name: 'display_name',
+        name: function(me) {
+			if (me.name) {
+				return me.name.display_name
+			}
+			return ""
+		},
         email: '=',
         location: 'country',
-        locale: 'language'
+		locale: 'locale',
+		avatar: 'profile_photo_url'
     }
 };
 module.exports = me;
