4b78bf50bcf91451a5ac845d25e4f48c4e81474c
Douglas Read <dreadjr@gmail.com>
2019-05-17 13:44:02 -0700

    feature: linkedin2 v2 email for profile and email

--- providers/linkedin2/me.js
+++ providers/linkedin2/me.js
@@ -1,19 +1,76 @@
 var me = {
-
-    url: '/v2/people/~:(id,first-name,last-name,headline,picture-url,email-address,public-profile-url,positions)?format=json',
+    fetch: [
+        {
+          url: '/v2/emailAddress?q=members&projection=(elements*(handle~))',
+          export: {
+            emails: function (result) {
+              return result;
+            }
+          }
+        },
+        {
+          url: '/v2/me?projection=(id,localizedFirstName,localizedLastName,localizedHeadline,profilePicture(displayImage~:playableStreams),vanityName)',
+          export: {
+            user: function (result) {
+              return result;
+            }
+          }
+        },
+        function (fetched_elts) {
+          return fetched_elts;
+        }
+    ],
     params: {},
     fields: {
-        id: 'id',
-        name: function(me) {
-            return me.firstName + ' ' + me.lastName;
+        id: function (me) {
+            return me.user.id || undefined;
         },
-        firstname: 'firstName',
-        lastname: 'lastName',
-        alias: 'screen_name',
-        bio: 'headline',
-        avatar: 'pictureUrl',
-        email: 'emailAddress',
-        url: 'publicProfileUrl'
+        name: function(me) {
+            var name = [];
+
+            if (!!me.user.localizedFirstName) {
+                name.push(me.user.localizedFirstName);
+            }
+
+            if (!!me.user.localizedLastName) {
+                name.push(me.user.localizedLastName);
+            }
+
+            return name.join(' ');
+        },
+        firstname: function (me) {
+            return me.user.localizedFirstName || undefined;
+        },
+        lastname: function (me) {
+            return me.user.localizedLastName || undefined;
+        },
+        alias: function (me) {
+            return me.user.vanityName || undefined;
+        },
+        bio: function (me) {
+            return me.user.localizedHeadline || undefined;
+        },
+        avatar: function(me) {
+            try {
+                return me.user.profilePicture['displayImage~'].elements.pop().identifiers.pop().identifier;
+            } catch (e) {
+                return undefined;
+            }
+        },
+        email: function(me) {
+            try {
+                return me.emails.elements.pop()['handle~'].emailAddress;
+            } catch (e) {
+                return undefined;
+            }
+        },
+        url: function(me) {
+            if (!me.user.vanityName) {
+                return undefined;
+            }
+
+            return 'https://www.linkedin.com/in/' + me.user.vanityName;
+        }
     }
 };
 
